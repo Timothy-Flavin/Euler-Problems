a5dc99d4db792551eed4eb3d6cf529646ddaf64f
Timothy Flavin <BinaryOwlizard@gmail.com>
2019-10-09 22:23:07 -0500

    at least the script made it this time

---
+++ Project1/Source.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+void eulerProblem7();
+void eulerProblem30();
+void eulerProblem26();
+int main() {
+	eulerProblem26();
+	std::cin.get();
+	return 0;
+}
+
+void eulerProblem26(){
+	int sequence[100000];
+	int sequenceLength = 0;
+	bool notInSequence = true;
+	int topSequenceLength = 0;
+	for (int i = 1; i < 1000; i++) {
+		std::cout << "onNumber: " << i << std::endl;
+		sequenceLength = 0;
+		notInSequence = true;
+		int divisor = 1;
+		int remainder = 0;
+		int topNumber = divisor / i;
+		while (notInSequence) {
+			topNumber = divisor / i;
+			remainder = divisor - i * topNumber;
+			if (remainder == 0) break;
+			else {
+				sequence[sequenceLength] = remainder;
+				divisor = remainder * 10;
+				for (int j = 0; j < sequenceLength; j++) {
+					if (remainder == sequence[j]) {
+						notInSequence = false;
+						
+						if (sequenceLength - j > topSequenceLength) {
+							std::cout << "highestNumber: " << i << "at: " << sequenceLength - j << std::endl;
+							topSequenceLength = sequenceLength - j;
+						}
+					}
+				}
+				sequenceLength++;
+			}
+		}
+	}
+	std::cout << topSequenceLength;
+}
+
+void eulerProblem7() {
+	int counter = 1;
+	int currentNum = 3;
+	bool notPrime = true;
+	const int primeToFind = 100001;
+	int primeList[primeToFind];
+	primeList[0] = 2;
+	while (counter<primeToFind) {
+		notPrime = false;
+		for (int i = 0; i < counter && primeList[i] <= sqrt(currentNum); i++) {
+			if (currentNum % primeList[i] == 0) {
+				notPrime = true;
+				i = currentNum;
+			}
+		}
+		if (!notPrime) {
+			primeList[counter] = currentNum;
+			counter++;
+		}
+		currentNum++;
+	}
+	std::cout << currentNum - 1 << std::endl;
+}
+
+void eulerProblem30() {
+	int answer = 0;
+	int power = 5;
+	for (int i = 2; i < pow(9, power)*(power+1); i++) {
+		int digitCounter = 0;
+		int temp = i;
+		for (int j = 0; j < power+1; j++) {
+			digitCounter += pow(temp % 10,power);
+			temp /= 10;
+		}
+		if (digitCounter == i) {
+			answer += digitCounter;
+		}
+	}
+	std::cout << "answer: " << answer << std::endl;
+}
