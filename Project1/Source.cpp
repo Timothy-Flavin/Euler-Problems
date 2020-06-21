5cea8f7bbdd42441fbc6a344205b1b9803eaf6ac
Timothy Flavin <Timothy-Flavin@gmail.com>
2020-06-21 18:46:26 -0500

    Finished Euler problem 17

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 void eulerProblem7();
 void eulerProblem30();
 void eulerProblem26();
@@ -54,7 +56,7 @@ void eulerProblem7() {
 	primeList[0] = 2;
 	while (counter<primeToFind) {
 		notPrime = false;
-		for (int i = 0; i < counter && primeList[i] <= sqrt(currentNum); i++) {
+		for (int i = 0; i < counter && primeList[i] <= std::sqrt(currentNum); i++) {
 			if (currentNum % primeList[i] == 0) {
 				notPrime = true;
 				i = currentNum;
@@ -72,11 +74,11 @@ void eulerProblem7() {
 void eulerProblem30() {
 	int answer = 0;
 	int power = 5;
-	for (int i = 2; i < pow(9, power)*(power+1); i++) {
+	for (int i = 2; i < std::pow(9, power)*(power+1); i++) {
 		int digitCounter = 0;
 		int temp = i;
 		for (int j = 0; j < power+1; j++) {
-			digitCounter += pow(temp % 10,power);
+			digitCounter += std::pow(temp % 10,power);
 			temp /= 10;
 		}
 		if (digitCounter == i) {
@@ -86,17 +88,49 @@ void eulerProblem30() {
 	std::cout << "answer: " << answer << std::endl;
 }
 
-void eulerProblem17() {
-	std::string[] onesPlace = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-	std::string[] teens = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-	int[9] oneePlaceLengths;
-	int[9] teensLengths;
 
-	for (int i = 0; i < 9; i++) {
-		onesPlaceLengths = onesPlace[i].length();
-		teensLengths = teens[i].length();
+void eulerProblem17() {
+	std::string onesWords[10] = {"","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	std::string teensWords[10] = {"ten","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	std::string tensWords[8] = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+	std::string hundredWord = "hundred";
+	std::string andWord = "and";
+	std::string oneThousand = "oneThousand";
+
+	int numberLength = 0;
+	int totalLength = 0;
+	for(int temp = 1; temp < 1000; temp++){
+		int ones = temp%10;
+		int tens = (temp/10)%10;
+		int hundreds = (temp/100)%10;
+		
+		
+		
+		numberLength=int(onesWords[hundreds].length() + (hundreds?(hundredWord.length()+((ones||tens)?andWord.length():0)):0));
+		std::cout<<temp<<": "<<"Hundreds place "<<hundreds<<", "<<onesWords[hundreds]<<", length: "<<numberLength<<std::endl; //", "<<((ones||tens)?andWord.length():0)
+		
+		//std::cout<<onesWords[hundreds] << hundreds?(hundredWord+ones?andWord:""):""<<" ";
+		switch(tens){
+			case 0:
+				numberLength+=onesWords[ones].length();
+				std::cout<<onesWords[ones];
+			break;
+			case 1:
+				numberLength+=teensWords[ones].length();
+				std::cout<<teensWords[ones];
+			break;
+			default:
+				numberLength+=tensWords[tens-2].length()+onesWords[ones].length();
+				std::cout<<tensWords[tens-2] << onesWords[ones] <<std::endl;
+			break;
+		}
+
+		std::cout<<numberLength<<std::endl;
+		totalLength+=numberLength;
+		//std::cout <<"ones place lengths "<<i<<": "<< onesPlace[0].length()<<std::endl;
+		//std::cout <<"teens place lengths "<<i<<": "<< teens[0].length()<<std::endl;
 	}
 
-	std::cout << onsePLaceLengths<<std::endl;
-	std::cout < teensLengths<<std::endl;
+	std::cout<<totalLength + oneThousand.length()<<std::endl;
+	
 }
