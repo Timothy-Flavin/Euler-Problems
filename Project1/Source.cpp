8d3c849106437c139bbe2bb394aca553b3b8adfd
Timothy-Flavin <binaryOwlizard@gmail.com>
2020-06-24 19:57:19 -0500

    did euler problem 8. This one was a little too easy. it could be made much faster by deviding by the last num and multiplying by the next one instead of doing all 13 but it is only 1000 numbers so brute force is fine. ill fix it after

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <algorithm>
 void eulerProblem7();
+void eulerProblem8();
 void eulerProblem30();
 void eulerProblem26();
 void eulerProblem17();
@@ -15,7 +16,7 @@ int calcTotient(int n, std::vector<int> primes);
 std::vector<int> getPrimes(int n);
 std::vector<int> getPrimes2(int n);
 int main() {
-	eulerProblem69();
+	eulerProblem8();
 	
 	std::cin.get();
 	return 0;
@@ -98,7 +99,6 @@ void eulerProblem30() {
 	std::cout << "answer: " << answer << std::endl;
 }
 
-
 void eulerProblem17() {
 	std::string onesWords[10] = {"","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 	std::string teensWords[10] = {"ten","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
@@ -143,7 +143,6 @@ void eulerProblem17() {
 
 	std::cout<<totalLength + oneThousand.length()<<std::endl;
 }
-
 
 int gcd(int a, int b)  
 {  
@@ -378,4 +377,19 @@ void testPrimesVsPrimes2(){
 		}
 	}
 	std::cout<<"done"<<std::endl;
+}
+
+void eulerProblem8(){
+	std::string numberString="7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+	unsigned long biggestNum = 0;
+	unsigned long tempNum = 0;
+	for(int i=0; i < numberString.length()-13; i++){
+		tempNum=1;
+		for(int j = 0; j < 13; j++){
+			tempNum*=int(numberString[i+j])-48;
+			std::cout<<"string val: "<<numberString[i+j]<<"num val: "<<int(numberString[i+j])-48<<std::endl;
+		}
+		if(tempNum>biggestNum) biggestNum=tempNum;
+	}
+	std::cout<<biggestNum<<std::endl;
 }
